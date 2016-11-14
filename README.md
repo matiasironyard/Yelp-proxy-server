de0e287f255a81fdef3401c80e92e9545538db97
Dan Dietz <danddietz@gmail.com>
2016-11-14 18:41:51 -0400

    sample server setup

---
+++ README.md
@@ -0,0 +1,5 @@
+# Sample Node Proxy Server
+
+This node server can be used to configure a proxy server for
+final projects that use 3rd Party APIs that don't support
+Access-Control-Allow-Origin header.
---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var request = require('request');
+var app = express();
+var cors = require('cors');
+
+app.use(cors()); //allows overriding cross origin policy (use npm install if needed)
+
+app.get('/test', function(req, res){ // listens for request on /api route
+ console.log('working!');
+ res.send('working!'); // if no errors, send the body of data back to front end
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log('Server running on port 3000');
+
+/* BreweryDB API Example */
+// app.get('/api', function(req, res){ // listens for request on /api route
+//   var lat = req.query.lat; // grabs lat and lng queries from the request object
+//   var lng = req.query.lng;
+//   request('https://api.brewerydb.com/v2/search/geo/point?lat=' + lat + '&lng=' + lng + '&type=beer&hasImages=Y&key=72a751214ab8b53056ac0a6d8376dc2d', function (error, response, body) { // api url
+//     if (!error && response.statusCode === 200) {
+//       console.log('beer');
+//       res.send(body); // if no errors, send the body of data back to front end
+//     }
+//    });
+// });
