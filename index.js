5c785d02bcd71c0c558f49e71d6a098e1b02f1b6
Matias Mariani <matiasironyard@gmail.com>
2016-12-04 17:14:09 -0500

    update

--- index.js
+++ index.js
@@ -39,10 +39,10 @@ app.get('/fusion', function(req, res){ // listens for request on /api route
  console.log('test api!');
  // res.send('working api!');
 
-var yelp = new Yelp({
-  token_type: "Bearer",
-  expires_in: 15551318,
-   access_token: "i565rg5Yi8NoRyCmfTHX5ngjnlRSCmkQkG0CHSibDWzBY2OJQd3LNU0WfEsf9XibRZAiCBriAohxLkjbsZ4IfqMi-al9M4xp9DYM9vGZM6MlyNW45Z8earlIU94pWHYx"
+var yelpFusion = new Yelp({
+  token_type: 'Bearer',
+  expires_in: '15551318',
+  access_token:' i565rg5Yi8NoRyCmfTHX5ngjnlRSCmkQkG0CHSibDWzBY2OJQd3LNU0WfEsf9XibRZAiCBriAohxLkjbsZ4IfqMi-al9M4xp9DYM9vGZM6MlyNW45Z8earlIU94pWHYx',
 });
 var phone = req.query.phone;
 
