07600820b239488da85a38517576899772498e82
Matias Mariani <matiasironyard@gmail.com>
2016-12-04 17:00:25 -0500

    added yelp fusion

--- index.js
+++ index.js
@@ -12,18 +12,38 @@ app.get('/test', function(req, res){ // listens for request on /api route
 });
 
 /* PUT YOUR CODE BETWEEN COMMENTS */
+// 
+// app.get('/phone', function(req, res){ // listens for request on /api route
+//  console.log('test api!');
+//  // res.send('working api!');
+//
+// var yelp = new Yelp({
+//   consumer_key: '-5M9jfXB_KlLZSry7N0y7g',
+//   consumer_secret: '-CsnsEpO-AIvCUgJkzzMFxz4n7U',
+//   token: 'XVoJD8K_7G7HFLBBEZswvl1u3idr7Ete',
+//   token_secret: '3O88cZubxXw_0FqMW4zLsSqCZFU',
+// });
+//
+// var phone = req.query.phone;
+//
+// yelp
+//   .phoneSearch({phone: phone})
+//   .then(function(data){
+//     res.send(data);
+//   }).catch(console.error);
+// });
 
-app.get('/api', function(req, res){ // listens for request on /api route
+/* PUT YOUR CODE BELOW THIS COMMENT */
+
+app.get('/phone', function(req, res){ // listens for request on /api route
  console.log('test api!');
  // res.send('working api!');
 
 var yelp = new Yelp({
-  consumer_key: '-5M9jfXB_KlLZSry7N0y7g',
-  consumer_secret: '-CsnsEpO-AIvCUgJkzzMFxz4n7U',
-  token: 'XVoJD8K_7G7HFLBBEZswvl1u3idr7Ete',
-  token_secret: '3O88cZubxXw_0FqMW4zLsSqCZFU',
+  token_type: "Bearer",
+  expires_in: 15551318,
+   access_token: "i565rg5Yi8NoRyCmfTHX5ngjnlRSCmkQkG0CHSibDWzBY2OJQd3LNU0WfEsf9XibRZAiCBriAohxLkjbsZ4IfqMi-al9M4xp9DYM9vGZM6MlyNW45Z8earlIU94pWHYx"
 });
-
 var phone = req.query.phone;
 
 yelp
@@ -32,6 +52,7 @@ yelp
     res.send(data);
   }).catch(console.error);
 });
+
 
 /* PUT YOUR CODE ABOVE THIS COMMENT */
 
