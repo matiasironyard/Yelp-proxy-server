1d020edf28ff761dcec9effb2452c1ede423f0cf
Matias Mariani <matiasironyard@gmail.com>
2016-12-06 21:16:54 -0500

    fixing fusion biz search

--- index.js
+++ index.js
@@ -82,12 +82,15 @@ var yelp = new YelpFusion({
 var business= req.query.business;
 console.log(business);
 
-yelp
-  .business(business)
-  .then(function(data){
-    res.send(data);
-  }).catch(console.error);
-});
+// yelp.business(business)
+//   .then(function(data){
+//     res.send(data);
+//   }).catch(console.error);
+// });
+
+yelp.business(business)
+.then(function (data) { console.log(data); })
+.catch(function (err) { console.error(err);});
 
 
 /* PUT YOUR CODE ABOVE THIS COMMENT */
